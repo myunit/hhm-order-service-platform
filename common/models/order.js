5c4f4246ed05b749ce879961e5eb3cfc6f40c24a
joneqian <qian.qing@aliyun.com>
2016-03-28 19:17:22 +0800

    order list

--- common/models/order.js
+++ common/models/order.js
@@ -69,6 +69,9 @@ module.exports = function (Order) {
         } else {
           delete res.IsSuccess;
           delete res.ErrorDescription;
+          if (res.PayMent !== '货到付款') {
+            res.PayMent = '在线付款';
+          }
           cb(null, {status: 1, order: res, msg: ''});
         }
       });
